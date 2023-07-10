73461739ad0708bad5a334079c7828c89c01c26b
Sandeep <sandeephborhade@gmail.com>
2023-07-10 13:16:01 -0400

    formatted work experience - react resume

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -4,15 +4,16 @@ function WorkExperience({ data }) {
   return (
     <section className="WorkExperience">
       <h2>Work Experience</h2>
-      <strong>
-        {data.position} <br />
-        {data.company} - {data.duration} <br />
-      </strong>
+      <div className="company">{data.company}</div>
+      <div className="position">{data.position}</div>
+      <div className="duration">{data.duration}</div>
+      <div>
       <ul>
         {data.responsibilities.map((responsibility, index) => (
           <li key={index}>{responsibility}</li>
         ))}
       </ul>
+      </div>
     </section>
   );
 }
